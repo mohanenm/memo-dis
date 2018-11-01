0533aa3ff6d1ff8974705fd682eb8173ac6ac821
Ethan Mohan <ethannmohan@gmail.com>
2018-11-01 13:55:45 -0500

    CMake Build

---
+++ build/CMakeFiles/mem-hash.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mem-hash.dir/mem-hash.c.o"
+  "bin/mem-hash.pdb"
+  "bin/mem-hash"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mem-hash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

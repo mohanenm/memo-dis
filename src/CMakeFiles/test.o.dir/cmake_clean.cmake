d907a785d46efff50b34e4c37505bbd74ef57efb
Ethan Mohan <ethannmohan@gmail.com>
2018-12-13 18:48:51 -0600

    minor make update

---
+++ src/CMakeFiles/test.o.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.o.dir/testBoss.c.o"
+  "bin/test.o.pdb"
+  "bin/test.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test.o.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
